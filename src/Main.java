cb1d34f7ca067ea0298f19bbbc7567602b01197b
yann <yannlefranc330@gmail.com>
2023-04-11 18:27:47 +0200

    Interraction the witcher

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+
+        //declaration de la variable
+        String userName;
+
+        // Enter username and press Enter
+        System.out.println("Entre ton jeux video");
+        userName = myObj.nextLine();
+
+        System.out.println("Mon jeux vidéo: " + userName);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
